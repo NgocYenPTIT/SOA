8c2cfa20083605e096804d2ba287e7ee2455de48
Ngoc Yen <dieutuyetnguyenthi@gmail.com>
2025-05-01 17:23:35 +0700

    Change fe-service to ui

--- services/ui/src/main/java/com/example/clientKTPM/Main.java
+++ services/ui/src/main/java/com/example/clientKTPM/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KienTrucPhanMemApplication {
+public class Main {
 
     public static void main(String[] args) {
 
