730a1f3eb9eded28f5e2200602fcfa1e299bd5c6
Ngoc Yen <dieutuyetnguyenthi@gmail.com>
2025-05-03 00:58:53 +0700

    detail semester

--- services/semester-service/src/main/java/com/example/semester_service/controller/SemesterController.java
+++ services/semester-service/src/main/java/com/example/semester_service/controller/SemesterController.java
@@ -2,6 +2,8 @@ package com.example.semester_service.controller;
 
 import com.example.semester_service.security.JwtTokenProvider;
 import com.example.semester_service.service.SemesterService;
+import org.springframework.http.ResponseEntity;
+import com.example.semester_service.model.Semester;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +15,15 @@ public class SemesterController {
     public SemesterController(JwtTokenProvider jwtTokenProvider, SemesterService semesterService) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.semesterService = semesterService;
+    }
+
+    @GetMapping("/semester/{id}")
+    public ResponseEntity<Semester> getSemesterById(@PathVariable Long id) {
+        Semester semester = semesterService.findSemesterById(id);
+        if (semester != null) {
+            return ResponseEntity.ok(semester);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- services/semester-service/src/main/java/com/example/semester_service/service/SemesterService.java
+++ services/semester-service/src/main/java/com/example/semester_service/service/SemesterService.java
@@ -1,17 +1,24 @@
 package com.example.semester_service.service;
 
+import com.example.semester_service.model.Semester;
 import com.example.semester_service.repository.SemesterRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SemesterService {
 
-    private final SemesterRepository userRepository;
+    private final SemesterRepository semesterRepository;
 
     @Autowired
-    public SemesterService(SemesterRepository userRepository) {
-        this.userRepository = userRepository;
+    public SemesterService(SemesterRepository semesterRepository) {
+        this.semesterRepository = semesterRepository;
+    }
+
+    public Semester findSemesterById(Long id) {
+        Optional<Semester> semesterOptional = semesterRepository.findById(id);
+        return semesterOptional.orElse(null);
     }
 
 }
